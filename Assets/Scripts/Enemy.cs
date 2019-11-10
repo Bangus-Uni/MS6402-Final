1580882bce172a02f67d09d218ed5d389a99ec2f
Bangus-Uni <47008683+Bangus-Uni@users.noreply.github.com>
2019-11-10 20:12:58 +0000

    Added Basic Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy: MonoBehaviour
+{
+    public float flMoveSpeed = 5;
+    private Rigidbody rbEnemy;
+
+    public GameObject goPC;
+
+    public float flRange;
+
+    private Vector3 v3MoveVelocity;
+
+    public Gun EnemyGun;
+
+    void Start() {
+        rbEnemy = GetComponent<Rigidbody>();
+        goPC = GameObject.FindGameObjectWithTag("PC");
+    }
+
+    void Update() {
+
+       transform.LookAt(new Vector3(goPC.transform.position.x, transform.position.y, goPC.transform.position.z));
+       EnemyGun.boolIsFiring = true;
+    }
+
+    private void FixedUpdate() {
+        transform.position = Vector3.MoveTowards(transform.position, goPC.transform.position, flMoveSpeed * Time.deltaTime);
+    }
+
+}
