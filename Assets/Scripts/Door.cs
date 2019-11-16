a64a9002af927ede306215cd3a7568c91b8fde48
Bangus-Uni <47008683+Bangus-Uni@users.noreply.github.com>
2019-11-16 22:46:57 +0000

    Added Basic World Generation

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool boolPosNeg1;
+    public bool boolPosNeg2;
+
+    public bool boolUseable = true;
+
+    public bool boolOpened = false;
+
+    public GameObject goRoom;
+
+    GameManager GM;
+
+
+    void Start()
+    {
+        GM = FindObjectOfType<GameManager>();
+        goRoom = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!boolOpened && other.tag == "PC") GM.GenerateRoom(boolPosNeg1, boolPosNeg2, goRoom.transform);
+        boolOpened = true;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] a_goRooms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateRoom(bool _boolPosNeg1, bool _boolPosNeg2, Transform _trRoom)
+    {
+        int _intDir1 = 0;
+        int _intDir2 = 0;
+        Vector3 v3NewRoomPos;
+        GameObject goNewRoom;
+
+        int intDice = Mathf.RoundToInt(Random.Range(0.5f, (a_goRooms.Length + 0.5f)));
+
+        Debug.Log("DiceRoll: " + intDice);
+
+        goNewRoom = a_goRooms[intDice - 1];
+
+
+        if (_boolPosNeg1 && _boolPosNeg2)
+        {
+            _intDir1 = 14;
+            _intDir2 = 14;
+            v3NewRoomPos = new Vector3(_trRoom.position.x + _intDir1, _trRoom.position.y, _trRoom.position.z + _intDir2);
+            GameObject _goNewRoom = Instantiate(goNewRoom, v3NewRoomPos, _trRoom.rotation);
+            _goNewRoom.transform.GetChild(4).gameObject.SetActive(false);
+        }
+
+        else if (_boolPosNeg1 && !_boolPosNeg2)
+        {
+            _intDir1 = 14;
+            _intDir2 = -14;
+            v3NewRoomPos = new Vector3(_trRoom.position.x + _intDir1, _trRoom.position.y, _trRoom.position.z + _intDir2);
+            GameObject _goNewRoom = Instantiate(goNewRoom, v3NewRoomPos, _trRoom.rotation);
+            _goNewRoom.transform.GetChild(3).gameObject.SetActive(false);
+        }
+
+        else if (!_boolPosNeg1 && _boolPosNeg2)
+        {
+            _intDir1 = -14;
+            _intDir2 = 14;
+            v3NewRoomPos = new Vector3(_trRoom.position.x + _intDir1, _trRoom.position.y, _trRoom.position.z + _intDir2);
+            GameObject _goNewRoom = Instantiate(goNewRoom, v3NewRoomPos, _trRoom.rotation);
+            _goNewRoom.transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        else if (!_boolPosNeg1 && !_boolPosNeg2)
+        {
+            _intDir1 = -14;
+            _intDir2 = -14;
+            v3NewRoomPos = new Vector3(_trRoom.position.x + _intDir1, _trRoom.position.y, _trRoom.position.z + _intDir2);
+            GameObject _goNewRoom = Instantiate(goNewRoom, v3NewRoomPos, _trRoom.rotation);
+            _goNewRoom.transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}
