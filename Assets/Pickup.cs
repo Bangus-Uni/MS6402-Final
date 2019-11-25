c7f720369f56219568af5b964c10d0485ccacee4
Ben Angus <u1725266@uel.ac.uk>
2019-11-25 18:01:06 +0000

    Added Pickup (In Progress)

---
+++ Assets/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public int intGunType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        intGunType = Mathf.RoundToInt(Random.Range(0.5f, 4.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
