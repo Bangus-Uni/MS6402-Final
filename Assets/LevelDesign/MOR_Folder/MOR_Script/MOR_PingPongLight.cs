4382d31e217152f001ca5de978df216302ce7bc2
Ben Angus <u1725266@uel.ac.uk>
2020-02-24 19:53:35 +0000

    Added Level Design Elements

---
+++ Assets/LevelDesign/MOR_Folder/MOR_Script/MOR_PingPongLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOR_PingPongLight : MonoBehaviour
+{
+    public float speed = 1;
+    private Light lightlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightlight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PingPongIntesity();
+    }
+
+    void PingPongIntesity()
+    {
+        lightlight.intensity = Mathf.PingPong(Time.time * speed, 1);
+    }
+}
+
